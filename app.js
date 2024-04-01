97fe1a9636684a85bc6d1f7eb1f418ca6c6803bf
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-04-01 19:07:28 +0530

    Passport Authentication Added

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError');
 const catchAsync=require('./utils/catchAsync')
 const Campground = require('./models/campground');
-const campgrounds=require('./routes/campgrounds')
+const campgroundsRoutes=require('./routes/campgrounds')
 const Review=require('./models/review')
-const reviews=require('./routes/reviews')
+const reviewsRoutes=require('./routes/reviews')
+const userRoutes=require('./routes/users')
+const User=require('./models/user')
 const session=require('express-session')
 const flash=require('connect-flash')
+const passport=require('passport')
+const LocalStrategy=require('passport-local')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -47,15 +51,24 @@ const sessionConfig={
 app.use(session(sessionConfig))
 app.use(flash())
 
-app.use((req,res,next)=>{
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
+app.use((req,res,next)=>{
+    res.locals.currentUser=req.user
     res.locals.success=req.flash('success')
     res.locals.error=req.flash('error')
     next()
 })
 
-app.use('/campgrounds',campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+
+
+app.use('/campgrounds',campgroundsRoutes)
+app.use('/campgrounds/:id/reviews', reviewsRoutes)
+app.use('/',userRoutes)
 
 
 app.get('/', (req, res) => {
